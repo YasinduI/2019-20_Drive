57ec52584859a0eab84245f6b27992bf1c776b2f
Sherman Yan <syan1689@gmail.com>
2019-10-23 16:21:14 -0700

    Update HolonomicTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicTeleop.java
@@ -1,11 +1,10 @@
-
+//shermans code//
 
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 
